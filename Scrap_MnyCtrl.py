a13d4527e5c50d98d77154a186dd6f5c36b11df1
Arun Dakua <arun.dakua@gmail.com>
2017-01-07 01:04:23 -0800

    Update Scrap_MnyCtrl.py

--- Scrap_MnyCtrl.py
+++ Scrap_MnyCtrl.py
@@ -1,12 +1,15 @@
-import mysql.connector
+#import mysql.connector
 import requests
 from bs4 import BeautifulSoup
 
+data_list = []
+transposed = [['year_mon'],['Mar 16'],['Mar 15'],['Mar 14'],['Mar 13'],['Mar 12']]
+transposed_row = []
 
-#page = requests.get('http://www.moneycontrol.com/financials/coalindia/balance-sheetVI/CI11#CI11')
-#soup_parent = BeautifulSoup(page.content,'html.parser')
+page = requests.get('http://www.moneycontrol.com/financials/coalindia/balance-sheetVI/CI11#CI11')
+soup_parent = BeautifulSoup(page.content,'html.parser')
 
-soup_parent = BeautifulSoup(open('page_content.html'),"lxml") #convert the html file to soup object
+#soup_parent = BeautifulSoup(open('page_content.html'),"lxml") #convert the html file to soup object
 tag_table = soup_parent.find_all('table') #first found all the table tag data as the values are present in table tag
 
 
@@ -15,13 +18,36 @@ tag_tr = soup_child1.find_all('tr') #find the tr tags which are present in table
 length_tag_tr = len(tag_tr)
 
 
-for i in range(0,length_tag_tr):
+a=[
+	[1,2],
+	[3,4],
+	[5,6]
+	]
+	
+print(a[1][0])
+
+for i in range(3,length_tag_tr):
 	soup_child2 = BeautifulSoup(str(tag_tr[i]),"lxml") #convert the tr tags to soup objects
 	tag_td = soup_child2.find_all('td') #find the td tag present within the tr tag
 	tag_td = list(filter(len,tag_td)) #filter out blank values from tag_td list to get the necessary data 
 	if len(tag_td) >= 5:
-		print(soup_child2.get_text(",",strip=True)) #get the text from td objects ,strip the space and concatenate with delimiter ,
+		#print(soup_child2.get_text(",",strip=True)) #get the text from td objects ,strip the space and concatenate with delimiter ,
+		temp = soup_child2.get_text(",",strip=True)
+		temp_list = temp.split(',')
+		transposed[0].append(temp_list[0])
+		transposed[1].append(temp_list[1])
+		transposed[2].append(temp_list[2])
+		transposed[3].append(temp_list[3])
+		transposed[4].append(temp_list[4])
+		transposed[5].append(temp_list[5])
+			
 
+for data in transposed:
+	print(data)
+		
+		
+
+'''
 cnx = mysql.connector.connect(user='root', password='root',host='127.0.0.1',database='mnyctrl')
 cursor = cnx.cursor()
 
@@ -32,59 +58,5 @@ cursor = cnx.cursor()
 #	print(vehicle_name)
 
 cnx.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'''
 
