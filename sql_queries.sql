3ba7aedeeca8569509df1fe33b06883ac8107023
Arun Dakua <arun.dakua@gmail.com>
2017-01-10 23:56:40 -0800

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -94,3 +94,109 @@ CREATE TABLE `profit_loss_detail` (
   CONSTRAINT `profit_loss_detail_ibfk_1` FOREIGN KEY (`stock_id`) REFERENCES `stock_list` (`stock_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
+
+
+create view balance_sheet as
+select
+bscd.bs_conso_detail_id,
+bscd.stock_id,
+bscd.Mon_Year,
+bscd.Total_Assets Assets,
+round((bscd.Total_NonCurrent_Liabilities + bscd.Total_Current_Liabilities),2) Liabilities
+from 
+balance_sheet_conso_detail bscd
+;
+
+
+create view profit_loss as 
+select 
+plcd.pl_conso_detail_id,
+plcd.stock_id,
+plcd.Mon_Year,
+plcd.Consolidated_Profit_Loss_After_MI_And_Associates pl_after_tax,
+plcd.Equity_Share_Dividend
+from
+profit_loss_conso_detail plcd 
+;
+
+create view ratio_vw as 
+select 
+r.ratio_id,
+r.stock_id,
+r.mon_year,
+r.Dividend__ShareRs
+from ratios r
+;
+
+
+CREATE TABLE stock_year_shares (
+  stock_yr_shrs_id mediumint(9) NOT NULL AUTO_INCREMENT,
+  stock_id mediumint(9) NOT NULL,
+  Year int(11) DEFAULT NULL,
+  No_of_Shares int(11) DEFAULT NULL,
+  PRIMARY KEY (stock_yr_shrs_id),
+  FOREIGN KEY (stock_id) REFERENCES stock_list (stock_id)
+) ;
+
+INSERT INTO stock_year_shares(stock_id,Year,No_of_Shares)
+VALUES(6066,2011,25711076);
+INSERT INTO stock_year_shares(stock_id,Year,No_of_Shares)
+VALUES(6066,2012,24512780);
+INSERT INTO stock_year_shares(stock_id,Year,No_of_Shares)
+VALUES(6066,2013,21181523);
+INSERT INTO stock_year_shares(stock_id,Year,No_of_Shares)
+VALUES(6066,2014,21317373);
+INSERT INTO stock_year_shares(stock_id,Year,No_of_Shares)
+VALUES(6066,2015,17701813);
+INSERT INTO stock_year_shares(stock_id,Year,No_of_Shares)
+VALUES(6066,2016,17719813);
+commit;
+
+
+select 
+stock_code,
+stock_name,
+current_year,
+calc_next_year,
+No_of_Shares,
+assets,
+Liabilities,
+shareholders_fund,
+(shareholders_fund/Assets) * 100 share_fund_prct,
+equity_val,
+(Equity_Val + Amt_Retain_from_EPS) Equity_Calc,
+pl_after_tax,
+eps,
+(ROE * (Equity_Val + Amt_Retain_from_EPS)) / 100 EPS_Calc,
+ROE,
+dividend,
+Amt_Retain_from_EPS
+from
+(
+select 
+sl.stock_code,
+sl.stock_name,
+bs.mon_year current_year,
+cast(bs.mon_year as signed) +1 calc_next_year,
+sys.No_Of_Shares ,
+bs.Assets ,
+bs.Liabilities ,
+round((bs.Assets - bs.Liabilities),2) ShareHolders_Fund,
+round((round((bs.Assets - bs.Liabilities),2) / sys.No_Of_Shares ) * 10000000,2) Equity_Val,
+pl.pl_after_tax,
+round((pl.pl_after_tax/sys.No_Of_Shares) * 10000000,2) EPS,
+(round((pl.pl_after_tax/sys.No_Of_Shares) * 10000000,2) / round((round((bs.Assets - bs.Liabilities),2) / sys.No_Of_Shares ) * 10000000,2)) * 100 ROE,
+r.Dividend__ShareRs Dividend,
+(round((pl.pl_after_tax/sys.No_Of_Shares) * 10000000,2) - r.Dividend__ShareRs) Amt_Retain_from_EPS
+from stock_list sl
+inner join stock_year_shares sys on sys.stock_id = sl.stock_id
+inner join balance_sheet bs on bs.stock_id = sl.stock_id and bs.mon_year = cast(sys.year as char)
+inner join profit_loss pl on pl.stock_id = sl.stock_id and bs.mon_year = pl.mon_year 
+inner join ratio_vw r on r.stock_id = sl.stock_id and r.mon_year = pl.mon_year
+where
+ r.mon_year in ('2012','2013','2014','2015','2016')
+) tbl;
+
+
+
+
